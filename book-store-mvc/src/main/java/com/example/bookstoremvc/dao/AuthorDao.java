450952b4ac6826ab137f094319387181b0b9f946
Than Zaw Htet <153487426+jrthanzawhtet@users.noreply.github.com>
2024-01-15 21:57:03 +0630

    Delete book-store-mvc directory

--- book-store-mvc/src/main/java/com/example/bookstoremvc/dao/AuthorDao.java
+++
@@ -1,7 +0,0 @@
-package com.example.bookstoremvc.dao;
-
-import com.example.bookstoremvc.entity.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorDao extends JpaRepository<Author,Integer> {
-}
--- book-store-mvc/src/main/java/com/example/bookstoremvc/dao/CategoryDao.java
+++
@@ -1,7 +0,0 @@
-package com.example.bookstoremvc.dao;
-
-import com.example.bookstoremvc.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryDao extends JpaRepository<Category,Integer> {
-}
